0e5450cbcf2afb88a0eb915247dfa849ac6720e4
CRT_HAO <m831718@gmail.com>
2022-01-10 00:02:16 +0800

    update maze test

--- Test/maze_test.py
+++ Test/maze_test.py
@@ -2,10 +2,10 @@ WIDTH = 12
 HEIGHT = 12
 
 directions = [
+    (0, 1), # Right
     (-1, 0), # Up
     (0, -1), # Left
     (1, 0), # Down
-    (0, 1) # Right
 ]
 
 def findStartPos(maze: list[list[chr]]) -> tuple[int, int]:
@@ -14,21 +14,33 @@ def findStartPos(maze: list[list[chr]]) -> tuple[int, int]:
             if maze[i][j] == 'X':
                 return (i, j)
 
-current_dir = 0
-def step(maze: list[list[chr]], pos: tuple[int, int]):
-    global current_dir
-    d = directions[current_dir]
-    print(d)
-    new_pos = (pos[0] + d[0], pos[1] + d[1])
-    if maze[new_pos[0]][new_pos[1]] != '#':
-        new_maze[new_pos[0]][new_pos[1]] = 'R'
-    else:
-        current_dir += 1
-        if current_dir > len(directions) - 1: current_dir = 0
-    if new_pos[1] != WIDTH - 1 and new_pos[1] != 0:
-        step(maze, new_pos)
-    # return new_pos
+def step(maze: list[list[chr]], start: tuple[int, int], end: tuple[int, int]):
+    pos = start
+    d = 0
+    while True:
+        printMaze(new_maze)
+        print()
+        if pos == end: return
+        # if maze[pos[0]][pos[1]] == 'X':
+        #     maze[pos[0]][pos[1]] = 'R'
+        maze[pos[0]][pos[1]] = 'R'
+        for i in range(len(directions)):
+            new_pos = (pos[0] + directions[d][0], pos[1] + directions[d][1])
+            print(new_pos)
+            if maze[new_pos[0]][new_pos[1]] != '#':
+                pos = new_pos
+                break
+            if i == len(directions) - 1: return
+            d += 1
+            if d > len(directions) - 1: d = 0
 
+def findStartPos(maze: list[list[chr]]) -> tuple[int, int]:
+    for i in range(len(maze)):
+        if maze[i][0] == 'X': return (i, 0)
+
+def findEndPos(maze: list[list[chr]]) -> tuple[int, int]:
+    for i in range(len(maze)):
+        if maze[i][len(maze[i]) - 1] == 'X': return (i, len(maze[i]) - 1)
 
 def printMaze(maze: list[list[chr]]):
     for a in maze:
@@ -47,10 +59,11 @@ if __name__ == '__main__':
     print(maze)
 
     start_pos = findStartPos(maze)
-    pos = start_pos
+    end_pos = findEndPos(maze)
 
     print(start_pos)
+    print(end_pos)
 
-    step(maze, pos)
+    step(maze, start_pos, end_pos)
 
     printMaze(new_maze)
