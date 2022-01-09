23014ce6a3ebd1f0708d4c63fa1d9a5fba78b6eb
CRT_HAO <m831718@gmail.com>
2022-01-10 00:02:51 +0800

    update maze

--- C1101227/C1101227Q01/main.c
+++ C1101227/C1101227Q01/main.c
@@ -7,30 +7,81 @@
 #define WIDTH 12
 #define HEIGHT 12
 
-#define UP {-1, 0}
-#define LEFT {0, -1}
-#define DOWN {1, 0}
-#define RIGHT {0, 1}
+typedef struct {
+    int x;
+    int y;
+} Pos;
 
-void findStartPos(char maze[WIDTH+1][HEIGHT+1], int* start_pos) {
-    bool stop = false;
-    for (int i = 0; i < HEIGHT && !stop; i++) {
-        for (int j = 0; j < WIDTH && !stop; j++) {
-            printf("%d ", maze[i][j]);
-            if (maze[i][j] == 'X') {
-                start_pos[0] = i;
-                start_pos[1] = j;
-                stop = true;
-            }
+Pos pt(int, int);
+void findStartPos(char[HEIGHT + 1][WIDTH + 1], Pos*);
+bool findEndPos(char[HEIGHT + 1][WIDTH + 1], Pos*);
+bool step(char[HEIGHT + 1][WIDTH + 1], Pos, Pos, int);
+void printMaze(char[HEIGHT + 1][WIDTH + 1]);
+Pos rotate(Pos, int);
+
+Pos pt(int x, int y) {
+    Pos p = { x, y };
+    return p;
+}
+
+void findStartPos(char maze[HEIGHT + 1][WIDTH + 1], Pos *start_pos) {
+    for (int i = 0; i < HEIGHT; i++) {
+        if (maze[i][0] == 'X') {
+            start_pos->x = i;
+            start_pos->y = 0;
+            return;
         }
-        printf("\n");
+    }
+}
+
+bool findEndPos(char maze[HEIGHT + 1][WIDTH + 1], Pos* end_pos) {
+    for (int i = 0; i < HEIGHT; i++) {
+        if (maze[i][WIDTH - 1] == 'X') {
+            end_pos->x = i;
+            end_pos->y = WIDTH - 1;
+            return true;
+        }
+    }
+    return false;
+}
+
+int dirs[][2] = {
+    {0, 1},
+    {-1, 0},
+    {0, -1},
+    {1, 0}
+};
+
+bool step(char maze[HEIGHT + 1][WIDTH + 1], Pos start, Pos end, int dir) {
+    if (maze[start.x][start.y] == 'X') {
+        maze[start.x][start.y] = 'R';
+        int d;
+        if (maze[end.x][end.y] != 'R' &&
+            !(step(maze, rotate(start, (d = (dir + 1) % 4)), end, d) ||
+            step(maze, rotate(start, (d = (dir + 4) % 4)), end, d) ||
+            step(maze, rotate(start, (d = (dir + 3) % 4)), end, d) )) {
+            //maze[start.x][start.y] = 'X';
+        }
+    }
+    return maze[end.x][end.y] == 'R';
+}
+
+Pos rotate(Pos pos, int dir) {
+    return pt(pos.x + dirs[dir][0], pos.y + dirs[dir][1]);
+}
+
+void printMaze(char maze[HEIGHT + 1][WIDTH + 1]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        printf("%s", maze[i]);
+        if(i != HEIGHT - 1) printf("\n");
     }
 }
 
 int main() {
-    int pos[] = { 0, 0 };
-    int start_pos[] = { 0, 0 };
-    char maze[WIDTH + 1][HEIGHT + 1] = { { '\0' } };
+    Pos pos = { 0, 0 };
+    Pos start_pos = { 0, 0 };
+    Pos end_pos = { 0, 0 };
+    char maze[HEIGHT + 1][WIDTH + 1] = { { '\0' } };
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH + 1; j++) {
             char c = getchar();
@@ -39,7 +90,15 @@ int main() {
             }
         }
     }
-    findStartPos(maze, start_pos);
-    printf("Start Pos: (%d, %d)", start_pos[0], start_pos[1]);
+    findStartPos(maze, &start_pos);
+    bool valid = findEndPos(maze, &end_pos);
+    valid &= step(maze, start_pos, end_pos, 0);
+    if (valid) {
+        printMaze(maze);
+    }
+    else {
+        printMaze(maze);
+        printf("\nThis maze has no solution");
+    }
     return 0;
 }
